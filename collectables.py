04cf7228c81506df56d6a8d1d1f9fb9e2f005d80
Caio Vidinha <44474107+caiovidinha@users.noreply.github.com>
2022-12-13 13:15:15 -0300

    Add files via upload

--- collectables.py
+++ collectables.py
@@ -3,8 +3,9 @@ import pygame
 class Collectable(pygame.sprite.Sprite):
     def __init__(self,pos,name):
         super().__init__()
-        caminho = 'assets/collects/' + name
+        caminho = 'assets/collects/' + name + '.png'
         self.image = pygame.image.load(caminho)
+        self.image = pygame.transform.scale(self.image,(20,20))
         self.rect = self.image.get_rect(topleft = pos)
         self.name = name
     def update(self,x_shift):
