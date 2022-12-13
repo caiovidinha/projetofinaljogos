65ae10bf1ca139adcd1a61fe4a6760b89d517323
Caio Vidinha <44474107+caiovidinha@users.noreply.github.com>
2022-12-13 11:44:18 -0300

    Add files via upload

---
+++ tiles/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset" tilewidth="26" tileheight="26" tilecount="27" columns="3">
+ <image source="tiles.png" width="78" height="234"/>
+</tileset>
